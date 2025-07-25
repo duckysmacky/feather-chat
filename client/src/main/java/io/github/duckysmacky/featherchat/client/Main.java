3ebb37fedf2504ef32cfaa44b147108329de709e
DuckySmacky <kriperoka@gmail.com>
2025-07-26 01:02:39 +0300

    Add basic client-to-server communication

--- client/src/main/java/io/github/duckysmacky/featherchat/client/Main.java
+++ client/src/main/java/io/github/duckysmacky/featherchat/client/Main.java
@@ -1,7 +1,38 @@
 package io.github.duckysmacky.featherchat.client;
 
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from 'client'");
+        String serverAddress = "127.0.0.1";
+        int serverPort = 8080;
+
+        System.out.printf("Trying to connect to %s:%s...%n", serverAddress, serverPort);
+        try (Socket server = new Socket(serverAddress, serverPort)) {
+            System.out.printf("Successfully connected to %s%n", server.getRemoteSocketAddress());
+
+            BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter serverIn = new BufferedWriter(new OutputStreamWriter(server.getOutputStream()));
+
+            String input;
+            while ((input = console.readLine()) != null) {
+                serverIn.write(input);
+                serverIn.newLine();
+                serverIn.flush();
+
+                if (input.equalsIgnoreCase("disconnect")) {
+                    System.out.println("Disconnecting from server...");
+                    break;
+                }
+            }
+
+            System.out.println("Connection terminated. Exiting...");
+        } catch (UnknownHostException e) {
+            System.err.println("Unable to connect to the server: invalid server IP provided");
+        } catch (IOException e) {
+            System.err.printf("Unable to connect to the server: %s%n", e.getMessage());
+        }
     }
 }
--- server/src/main/java/io/github/duckysmacky/featherchat/server/Main.java
+++ server/src/main/java/io/github/duckysmacky/featherchat/server/Main.java
@@ -1,7 +1,36 @@
 package io.github.duckysmacky.featherchat.server;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from 'server'");
+        int serverPort = 8080;
+
+        System.out.printf("Creating a server on %s...%n", serverPort);
+        try (ServerSocket server = new ServerSocket(serverPort)) {
+            System.out.printf("Listening for clients on %s%n", serverPort);
+
+            Socket client = server.accept();
+            System.out.printf("Successfully connected to %s%n", client.getRemoteSocketAddress());
+
+            BufferedReader clientOut = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+            String message;
+            while ((message = clientOut.readLine()) != null) {
+                if (message.equalsIgnoreCase("disconnect")) {
+                    System.out.println("Client requested disconnection. Closing client connection...");
+                    break;
+                }
+
+                System.out.printf("[client] %s%n", message);
+            }
+
+            client.close();
+            System.out.println("Connection terminated. Closing server...");
+        } catch (IOException e) {
+            System.out.printf("Unable to create server: %s%n", e.getMessage());
+        }
     }
 }
