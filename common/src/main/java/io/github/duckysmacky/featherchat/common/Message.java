9b7949848bc6c3c4b8899a36bc21fa4d889375e1
DuckySmacky <kriperoka@gmail.com>
2025-07-29 22:54:22 +0300

    Add factory methods for Message, replace string-based ID for UUID

--- common/src/main/java/io/github/duckysmacky/featherchat/common/Message.java
+++ common/src/main/java/io/github/duckysmacky/featherchat/common/Message.java
@@ -5,47 +5,45 @@ import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.NoSuchElementException;
 import java.util.StringTokenizer;
+import java.util.UUID;
 
 public class Message {
     private MessageType type;
-    private String senderId;
+    private UUID senderId;
     private LocalDateTime timeSent;
-    private boolean received;
     private String content;
 
-    public Message(String senderId, String content) {
-        this.type = getMessageType(content);
-        this.senderId = senderId;
-        this.timeSent = LocalDateTime.now();
-        this.received = false;
-        this.content = content;
-    }
-
-    private Message(MessageType type, String senderId, LocalDateTime timeSent, boolean received, String content) {
+    private Message(MessageType type, UUID senderId, LocalDateTime timeSent, String content) {
         this.type = type;
         this.senderId = senderId;
         this.timeSent = timeSent;
-        this.received = received;
         this.content = content;
     }
 
-    private MessageType getMessageType(String content) {
-        if (content.equalsIgnoreCase("disconnect"))
-            return MessageType.DISCONNECT;
+    /// Creates a new instance of a TEXT message
+    public static Message textMessage(UUID senderId, String content) {
+        return new Message(MessageType.TEXT, senderId, LocalDateTime.now(), content);
+    }
 
-        return MessageType.TEXT;
+    /// Creates a new instance of a CONNECT message
+    public static Message connectMessage(UUID senderId) {
+        return new Message(MessageType.CONNECT, senderId, LocalDateTime.now(), "Requesting connection");
+    }
+
+    /// Creates a new instance of a DISCONNECT message
+    public static Message disconnectMessage(UUID senderId) {
+        return new Message(MessageType.DISCONNECT, senderId, LocalDateTime.now(), "Requesting disconnection");
     }
 
     /// Parses the provided payload and returns the message. Will throw an exception if the parsing of the payload will
     /// fair
     ///
-    /// Format: `<type|sender_id|time_sent|received|content>``
+    /// Format: `<type|sender_id|time_sent|content>``
     /// Rules:
     /// - Fields must not be null
     /// - Field values must not contain '|'
     /// - Message is surrounded by angle brackets (`<...>`)
     /// - DateTime is formatted as ISO-8601 string (`2025-07-29T21:15:00`)
-    /// - `received` is lowercase "true"/"false"
     ///
     /// @throws InvalidPayloadException if the payload structure is invalid (e.g. missing required delimiters)
     /// @throws IllegalArgumentException if the payload contains invalid data (e.g. invalid enum type or invalid time
@@ -64,12 +62,11 @@ public class Message {
         StringTokenizer tokenizer = new StringTokenizer(payload.substring(start + 1, end), "|");
         try {
             MessageType type = MessageType.valueOf(tokenizer.nextToken());
-            String senderId = tokenizer.nextToken();
+            UUID senderId = UUID.fromString(tokenizer.nextToken());
             LocalDateTime timeSent = LocalDateTime.parse(tokenizer.nextToken());
-            boolean received = Boolean.parseBoolean(tokenizer.nextToken());
             String content = tokenizer.nextToken();
 
-            return new Message(type, senderId, timeSent, received, content);
+            return new Message(type, senderId, timeSent, content);
         } catch (NoSuchElementException e) {
             throw new InvalidPayloadException("Payload missing fields");
         }
@@ -79,27 +76,19 @@ public class Message {
     ///
     /// Custom payload format: `<type|sender_id|time_sent|received|content>`
     public byte[] intoPayload() {
-        return String.format("<%s|%s|%s|%s|%s>", type, senderId, timeSent, received, content).getBytes(StandardCharsets.UTF_8);
-    }
-
-    public void markRecieved() {
-        this.received = true;
+        return String.format("<%s|%s|%s|%s>", type, senderId, timeSent, content).getBytes(StandardCharsets.UTF_8);
     }
 
     public MessageType getType() {
         return type;
     }
 
-    public String getSenderId() {
+    public UUID getSenderId() {
         return senderId;
     }
 
     public LocalDateTime getTimeSent() {
         return timeSent;
-    }
-
-    public boolean isReceived() {
-        return received;
     }
 
     public String getContent() {
@@ -108,9 +97,12 @@ public class Message {
 
     @Override
     public String toString() {
-        if (type == MessageType.TEXT)
-            return String.format("[%s] <%s> %s", timeSent.toLocalTime().truncatedTo(ChronoUnit.SECONDS), senderId, content);
+        String time = timeSent.toLocalTime().truncatedTo(ChronoUnit.SECONDS).toString();
+        String id = senderId.toString().substring(0, 8);
 
-        return String.format("[%s] <%s> {%s}", timeSent.toLocalTime().truncatedTo(ChronoUnit.SECONDS), senderId, type.toString().toUpperCase());
+        if (type == MessageType.TEXT)
+            return String.format("[%s] <%s> %s", time, id, content);
+
+        return String.format("[%s] <%s> {%s}", time, id, type.toString().toUpperCase());
     }
 }
